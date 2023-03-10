34b16d3508a12d8083141ff9e82b624bbd56ca0e
josuechevezUEES <josue.chevez@uees.edu.sv>
2023-03-10 09:00:43 -0600

    Laravel español

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => '<b>UEES</b>LTE',
+    'logo' => '<b>UEES</b> TRANSPORTE',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
